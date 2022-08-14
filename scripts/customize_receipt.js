e486884adced9df160d35db33fddf205fe996224
NatKwok <natkwok902@gmail.com>
2022-08-14 19:10:29 -0400

    added confirmation page

---
+++ scripts/customize_receipt.js
@@ -0,0 +1,5 @@
+    //Show results
+    let full_name = document.getElementById('full_name').value;
+    let email = document.getElementById('email').value;
+    let DOE = document.getElementById('DOE').value;
+    let total_guests = document.getElementById('total_guests').value;
