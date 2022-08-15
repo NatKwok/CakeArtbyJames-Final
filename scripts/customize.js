c880720d4324cb6920db61592c32e1ecb7eca4b0
NatKwok <natkwok902@gmail.com>
2022-08-14 22:47:25 -0400

    linked scripts

--- scripts/customize.js
+++ scripts/customize.js
@@ -1,5 +1,4 @@
 
-
 function validation() {
 
     let full_name = $("#full_name");
@@ -8,31 +7,35 @@ function validation() {
     let total_guests = $("#total_guests");
 
     if (full_name == null || full_name.val("") || full_name.val("Type in Full Name (Firstname, Lastname)") ) {
+<<<<<<< Updated upstream
         console.log("passed")
+=======
+        console.log("passed full name")
+>>>>>>> Stashed changes
         alert("Please provide your full name");
         full_name.focus();
         return false;
         
     }
 
-    else if (email == null || email.value == "" || email.value == "Enter your email address") {
-        console.log("passed")
+    else if (email == null || email.val("") || email.val("Enter your email address") ) {
+        console.log("passed email")
         alert("Please provide your email");
         email.focus();
         return false;
         
     }
 
-    else if (DOE == null || DOE.value == "") {
-        console.log("passed")
+    else if (DOE == null || DOE.val("") ) {
+        console.log("passed DOE")
         alert("Please set a date");
         DOE.focus();
         return false;
         
     }
 
-    else if (total_guests == null || total_guests.value == "") {
-        console.log("passed")
+    else if (total_guests == null || total_guests.val("") || total_guests.val("Enter number of guests so we can prepare the cake size") ) {
+        console.log("passed guests")
         alert("Please provide the total number of guests");
         total_guests.focus();
         return false;
